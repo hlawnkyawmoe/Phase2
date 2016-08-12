86f7e2518dc50164baecc467f1e91aef17bd16e6
Hlawn Kyaw <Hlawn Kyaw>
2016-08-12 14:45:04 +0800

    commit

---
+++ phase2/UNOServer/src/java/sa42/team2/uno/RESTAppConfig.java
@@ -0,0 +1,9 @@
+package sa42.team2.uno;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RESTAppConfig extends Application{
+    
+}
